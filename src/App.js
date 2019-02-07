b1891ada008106240a11521ca17e8dc83e9ff9e9
Daniel Ors <do7811@gmail.com>
2019-02-07 10:31:16 -0500

    starting point for refactoring trelloyes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import './App.css';
 import List from './List';
 
 class App extends Component {
+
+  // add store to a new state storage here so that all details can be passed down to children
+
   static defaultProps = {
     store: {
       lists: [],
       allCards: {}
     }
   };
+
+  // handle delete button click function
+  // from instructions - how to remove key value pairs from an object
+
+  // function omit(obj, keyToOmit) {
+  //   return Object.entries(obj).reduce(
+  //     (newObj, [key, value]) =>
+  //         key === keyToOmit ? newObj : {...newObj, [key]: value},
+  //     {}
+  //   );
+  // }
+  
+
+  // handle random card button click function - from assignment instructions
+  // const newRandomCard = () => {
+  //   const id = Math.random().toString(36).substring(2, 4)
+  //     + Math.random().toString(36).substring(2, 4);
+  //   return {
+  //     id,
+  //     title: `Random Card ${id}`,
+  //     content: 'lorem ipsum',
+  //   }
+  // }
 
   render() {
     const {store} = this.props;
